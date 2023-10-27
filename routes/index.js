6b224bdf44d5e46d6c98ee059a23143cd5b1034f
jl-welch <jlw.dev@outlook.com>
2023-10-27 16:07:48 +0100

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.post("/contact", async function(req, res, next) {
+  try {
+    const { name, email, message } = req.body;
+  
+    const transporter = nodemailer.createTransport({
+      host: "mail.privateemail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject: `Message from ${email}: ${name}`,
+      text: message,
+    };
+  
+    await transporter.sendMail(mailOptions);
+  
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false });
+  }
+});
+
 
 module.exports = router;
